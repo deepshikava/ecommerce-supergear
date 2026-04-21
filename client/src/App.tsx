286c2ed021c11caac4a2ea12b37cdddf8aab5cbd
JARVIS\deeps <deepshika2407@gmail.com>
2026-04-21 23:39:35 +0800

    client - Initial setup

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import Container from "./ui/Container";
+import Footer from "./ui/Footer";
+import Header from "./ui/Header";
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <Container />
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
---
+++ client/src/ui/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { logo } from "../assets";
+import { IoClose, IoSearchOutline } from "react-icons/io5";
+
+const Header = () => {
+  return <div>Header</div>;
+};
+
+export default Header;
