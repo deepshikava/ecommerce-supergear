dfcd045d8de71143e13bb4e92f2f109180fe713a
JARVIS\deeps <deepshika2407@gmail.com>
2026-04-22 10:32:20 +0800

    client - Creating Route Setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,9 @@
 import Container from "./ui/Container";
-import Footer from "./ui/Footer";
-import Header from "./ui/Header";
 
 function App() {
   return (
     <main>
-      <Header />
       <Container />
-      <Footer />
     </main>
   );
 }
