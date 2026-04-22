570b17673bc61072acb9dc7a0423be23b9155eda
JARVIS\deeps <deepshika2407@gmail.com>
2026-04-23 02:27:56 +0800

    admin - dynamic route creation

--- admin/index.mjs
+++ admin/index.mjs
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import { fileURLToPath } from "url";
 import path from "path";
+import { readdirSync } from "fs";
 
 const app = express();
 
@@ -14,9 +15,17 @@ app.use(express.json());
 const _filePath = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(_filePath);
 
+const routePath = path.resolve(__dirname, "routes");
+
+const routeFiles = readdirSync(routePath);
+routeFiles.map(async (file) => {
+  const routeModule = await import(`./routes/${file}`);
+  app.use("/", routeModule.default);
+});
+
 app.get("/", (req, res) => {
   // Logic to perform data backup goes here
-  res.send("Backend initiated successfully!");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
